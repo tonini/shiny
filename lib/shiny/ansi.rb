bc2a928b6ab9b1fd5d20f7ce22e9c88fd44c761e
Samuel Tonini <tonini.samuel@gmail.com>
2010-11-15 21:19:08 +0100

    use symbols instead strings for performance

--- lib/shiny/ansi.rb
+++ lib/shiny/ansi.rb
@@ -21,43 +21,43 @@ module Shiny
 
     # ansi escape sequences list
     CODES = {
-      'black'             =>   "\e[30m",
-      'red'               =>   "\e[31m",
-      'green'             =>   "\e[32m",
-      'yellow'            =>   "\e[33m",
-      'blue'              =>   "\e[34m",
-      'magenta'           =>   "\e[35m",
-      'cyan'              =>   "\e[36m",
-      'white'             =>   "\e[37m",
-      'bright_black'      =>   "\e[90m",
-      'bright_red'        =>   "\e[91m",
-      'bright_green'      =>   "\e[92m",
-      'bright_yellow'     =>   "\e[93m",
-      'bright_blue'       =>   "\e[94m",
-      'bright_magenta'    =>   "\e[95m",
-      'bright_cyan'       =>   "\e[96m",
-      'bright_white'      =>   "\e[97m",
-      'on_black'          =>   "\e[40m",
-      'on_red'            =>   "\e[41m",
-      'on_green'          =>   "\e[42m",
-      'on_yellow'         =>   "\e[43m",
-      'on_blue'           =>   "\e[44m",
-      'on_magenta'        =>   "\e[45m",
-      'on_cyan'           =>   "\e[46m",
-      'on_white'          =>   "\e[47m",
-      'on_bright_black'   =>  "\e[100m",
-      'on_bright_red'     =>  "\e[101m",
-      'on_bright_green'   =>  "\e[102m",
-      'on_bright_yellow'  =>  "\e[103m",
-      'on_bright_blue'    =>  "\e[104m",
-      'on_bright_magenta' =>  "\e[105m",
-      'on_bright_cyan'    =>  "\e[106m",
-      'on_bright_white'   =>  "\e[107m",
-      'reset'             =>    "\e[0m",
-      'bold'              =>    "\e[1m",
-      'underline'         =>    "\e[4m",
-      'negative'          =>    "\e[7m",
-      'blink'             =>    "\e[5m"
+      :black             =>   "\e[30m",
+      :red               =>   "\e[31m",
+      :green             =>   "\e[32m",
+      :yellow            =>   "\e[33m",
+      :blue              =>   "\e[34m",
+      :magenta           =>   "\e[35m",
+      :cyan              =>   "\e[36m",
+      :white             =>   "\e[37m",
+      :bright_black      =>   "\e[90m",
+      :bright_red        =>   "\e[91m",
+      :bright_green      =>   "\e[92m",
+      :bright_yellow     =>   "\e[93m",
+      :bright_blue       =>   "\e[94m",
+      :bright_magenta    =>   "\e[95m",
+      :bright_cyan       =>   "\e[96m",
+      :bright_white      =>   "\e[97m",
+      :on_black          =>   "\e[40m",
+      :on_red            =>   "\e[41m",
+      :on_green          =>   "\e[42m",
+      :on_yellow         =>   "\e[43m",
+      :on_blue           =>   "\e[44m",
+      :on_magenta        =>   "\e[45m",
+      :on_cyan           =>   "\e[46m",
+      :on_white          =>   "\e[47m",
+      :on_bright_black   =>  "\e[100m",
+      :on_bright_red     =>  "\e[101m",
+      :on_bright_green   =>  "\e[102m",
+      :on_bright_yellow  =>  "\e[103m",
+      :on_bright_blue    =>  "\e[104m",
+      :on_bright_magenta =>  "\e[105m",
+      :on_bright_cyan    =>  "\e[106m",
+      :on_bright_white   =>  "\e[107m",
+      :reset             =>    "\e[0m",
+      :bold              =>    "\e[1m",
+      :underline         =>    "\e[4m",
+      :negative          =>    "\e[7m",
+      :blink             =>    "\e[5m"
     }
 
     # list of available ansi colors
@@ -68,10 +68,10 @@ module Shiny
 
     # generate instance methods
     CODES.each do |code, value|
-      next if code == 'reset'
-      reset = CODES['reset']
+      next if code == :reset
+      reset = CODES[:reset]
       class_eval <<-DEF
-        def #{code}
+        def #{code.to_s}
           @string = "#{value}" + @string + "#{reset}"
           self
         end
